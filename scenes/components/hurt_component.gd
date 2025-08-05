1c5b97656b58210858f156017ec457babe593477
Igor Carvalho <igorecarvalhopj@gmail.com>
2025-08-05 19:48:21 -0300

    added shader shaker effect to hurted trees

--- scenes/components/hurt_component.gd
+++ scenes/components/hurt_component.gd
@@ -8,6 +8,6 @@ signal hurt
 
 func _on_area_entered(area: Area2D) -> void:
 	var hit_component = area as HitComponent
-	
+	print(hit_component.current_tool, " tool: ", tool)
 	if tool == hit_component.current_tool:
 		hurt.emit(hit_component.hit_damage)
