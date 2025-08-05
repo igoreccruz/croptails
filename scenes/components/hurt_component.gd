746a955a57e30ce73f40d0cd3cdf3346ffe11f35
Igor Carvalho <igorecarvalhopj@gmail.com>
2025-08-05 20:36:48 -0300

    created rocks to collect stones

--- scenes/components/hurt_component.gd
+++ scenes/components/hurt_component.gd
@@ -8,6 +8,5 @@ signal hurt
 
 func _on_area_entered(area: Area2D) -> void:
 	var hit_component = area as HitComponent
-	print(hit_component.current_tool, " tool: ", tool)
 	if tool == hit_component.current_tool:
 		hurt.emit(hit_component.hit_damage)
