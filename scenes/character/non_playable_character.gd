dfd92a99531e20bec7a9f8ef8d575e35259d5749
Igor Carvalho <igorecarvalhopj@gmail.com>
2025-08-06 21:42:36 -0300

    created npc chicken and navigations

---
+++ scenes/character/non_playable_character.gd
@@ -0,0 +1,9 @@
+class_name NonPlayableCharacter
+extends CharacterBody2D
+
+
+@export var min_walk_cicle: int = 2
+@export var max_walk_cicle: int = 6
+
+var walk_cicles: int
+var current_walk_cicle: int
